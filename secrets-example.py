71f1f5c489f1e01725d04fb779fcf549a712f05a
Mohamed Oun <mohamed3on@gmail.com>
2017-07-20 16:23:12 +0200

    remove secrets file and add example
    Revert "remove secrets file"
    
    This reverts commit f6a574366955168058f43b3182606fd8e940a180.
    
    add secrets example

---
+++ secrets-example.py
@@ -0,0 +1,7 @@
+# rename this to secrets.py, and fill the variables with the relevant info (username is your twitter handle)
+
+consumer_key = "**************************"
+consumer_secret = "**************************"
+access_token = "**************************"
+access_token_secret = "**************************"
+username = "**************************"
