471777d839250d05da566abb1aa52d0d3fb9d755
Mohamed Oun <mohamed3on@gmail.com>
2018-02-27 09:54:20 +0100

    Change list name, encapsulate into a function, only save popular places

--- popularfriendslocations.py
+++ popularfriendslocations.py
@@ -1,36 +1,49 @@
 import json
 import operator
-
+import re
 import tweepy
+import os
 
 import secrets
 
-list_name = "coding"
+list_name = "code"
 
 auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 myfriends = api.friends(count=200)
-list = api.list_members("mohamed3on", list_name, count=5000)
+thelist = api.list_members("mohamed3on", list_name, count=5000)
 locations = {}
-for friend in myfriends:
-    location = friend.location
-    for l in location.split(','):
-        l = l.strip()
-        if l in locations:
-            locations[l] += 1
-        else:
-            locations[l] = 1
-for member in list:
-    location = member.location
-    for l in location.split(','):
-        l = l.strip()
-        if l in locations:
-            locations[l] += 1
-        else:
-            locations[l] = 1
-locations = {k: v for k, v in locations.items() if v > 2 and k != ''}
-mostcommon = sorted(locations.items(), key=operator.itemgetter(1), reverse=True)
+
+
+def addlocations(thelist, locations):
+
+    for member in thelist:
+        location = member.location
+        if location != '':
+            fulllocation = "\'" + location + "\'"
+            all_locations = re.split(', | / ( ) & +', location)
+            if len(all_locations) > 1:
+                all_locations.append(fulllocation)
+            for l in all_locations:
+                if l is None:
+                    continue
+                l = l.strip()
+                if l in locations:
+                    locations[l] += 1
+                else:
+                    locations[l] = 1
+
+    return locations
+
+
+locations = addlocations(thelist, locations)
+locations = addlocations(myfriends, locations)
+
+locations = {k: v for k, v in locations.items() if v >= locations['Egypt']}
+mostcommon = sorted(locations.items(),
+                    key=operator.itemgetter(1), reverse=True)
 print(mostcommon)
 with open('commonlocations.json', 'w') as fp:
     json.dump(mostcommon, fp)
+os.system('commonlocations.json')
