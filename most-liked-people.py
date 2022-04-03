aefbc1d3ea2441fc8466d9081910c1f5eb9103b9
Mohamed Oun <mohamed3on@gmail.com>
2022-04-03 20:59:58 +0200

    update most liked people script

--- most-liked-people.py
+++ most-liked-people.py
@@ -8,20 +8,31 @@ import secrets
 auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
 def get_most_liked_people():
+    seen_ids = set()
     people = {}
-    for page in range(1,60):
-        likes = api.favorites(page=page)
+    last_id = None
+    while True:
+        likes = api.get_favorites(count=200, max_id=last_id)
+
+        if len(likes) == 1 or likes[1].id in seen_ids:
+            break
+
         for x in likes:
+            if x.id in seen_ids:
+                continue
+
             user = x.user.screen_name
             if user in people:
                 people[user] += 1
             else:
                 people[user] = 1
 
+            seen_ids.add(x.id)
+            last_id = x.id
 
     people = {k: v for k, v in people.items() if v > 5}
 
@@ -31,5 +42,6 @@ def get_most_liked_people():
     with open('./most-liked-people.json', 'w') as fp:
         json.dump(mostcommon, fp)
     os.system('code ./most-liked-people.json')
+
 
 get_most_liked_people()
